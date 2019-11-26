5b9a972e590a16f7db30e1504a308957a838a07d
db1995 <448099205@qq.com>
2019-11-26 12:06:26 +0800

    Add .js file

---
+++ src/main/resources/static/js/coco.js
@@ -0,0 +1,18 @@
+$(function () {
+    window.onresize = function () {
+        $('.chatarea').css("height", window.innerHeight * 0.6);
+    }
+    resize();
+});
+
+function getTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    return h + ':' + (m > 9 ? m : "0" + m) + ':' + (s > 9 ? s : "0" + s);
+}
+
+function resize() {
+    $('.chatarea').css("height", window.innerHeight * 0.6);
+}
